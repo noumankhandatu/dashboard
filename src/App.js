2e188df0e38c521a29c71aa543e8d1c2cffa4a0b
nouman <noumankhan619.915@gmail.com>
2022-04-20 04:51:35 -0700

    theme

--- src/App.js
+++ src/App.js
@@ -1,33 +1,50 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import { ThemeProvider, createTheme } from "@material-ui/core";
-import Switch from "@material-ui/core/Switch";
 import Routing from "./router";
-import "./App.css";
 function App() {
-  const [mode, setMode] = useState(true);
+  const mode = useSelector((state) => state.movies.selectedMovieShow);
   const theme = createTheme({
     palette: {
       color: {
-        main: mode ? "black" : "white",
+        main: mode ? "#000000" : "#FFFFFF",
+        primary: mode ? "#4C4C4C" : "#FFFFFF",
+        secondary: mode ? "#9FA2B4" : "#FFFFFF",
+        alpha: mode ? "#FFFFFF" : "#000000",
       },
       backgroundColor: {
-        main: mode ? "white" : "black",
+        main: mode ? "#FFFFFF" : "#000000",
+      },
+      icon: {
+        main: mode ? "#FFE9D" : "#FFFFFF",
+        primary: mode ? "#D3EDE0" : "#FFFFFF",
+        secondary: mode ? "#CFEEFF" : "#FFFFFF",
+        alpha: mode ? "#F3E5F5" : "#FFFFFF",
+        beta: mode ? "#FFFFC7" : "#FFFFFF",
+        gamma: mode ? "#F7D8DE" : "#FFFFFF",
+        hexa: mode ? "#FFE2E4" : "#FFFFFF",
       },
     },
     typography: {
-      fontFamily: "Ubuntu",
+      fontFamily: "Inter",
+      h1: {
+        fontSize: "30px",
+        fontWeight: 600,
+      },
+      h2: {
+        fontSize: "15px",
+        fontWeight: 600,
+      },
+      h3: {
+        fontSize: "14px",
+        fontWeight: 700,
+      },
     },
   });
 
   return (
     <ThemeProvider theme={theme}>
       <Routing />
-      <Switch
-        checked={mode}
-        onChange={() => {
-          setMode(!mode);
-        }}
-      />
     </ThemeProvider>
   );
 }
